095ff6582d9f95ce37c9d49cfcfab1c4c001b96e
DrMint <thomas@barillot.net>
2022-03-28 11:57:58 +0200

    Added a sitemap system

---
+++ next-sitemap.js
@@ -0,0 +1,28 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_URL_SELF && "https://accords-library.com",
+  generateRobotsTxt: true,
+  alternateRefs: [
+    {
+      href: `${process.env.NEXT_PUBLIC_URL_SELF}/en/`,
+      hreflang: "en",
+    },
+    {
+      href: `${process.env.NEXT_PUBLIC_URL_SELF}/fr/`,
+      hreflang: "fr",
+    },
+    {
+      href: `${process.env.NEXT_PUBLIC_URL_SELF}/ja/`,
+      hreflang: "ja",
+    },
+    {
+      href: `${process.env.NEXT_PUBLIC_URL_SELF}/es/`,
+      hreflang: "es",
+    },
+    {
+      href: `${process.env.NEXT_PUBLIC_URL_SELF}/pt-br/`,
+      hreflang: "pt-br",
+    },
+  ],
+  exclude: ["/en/*", "/fr/*", "/ja/*", "/es/*", "/pt-br/*"],
+};

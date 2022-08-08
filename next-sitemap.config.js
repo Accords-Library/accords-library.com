918b9b85029cc3bae2a1c94da6c6649cc6721455
DrMint <thomas@barillot.net>
2022-08-08 20:55:26 +0200

    Black listed some pages from indexing

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -8,21 +8,21 @@ module.exports = {
       hreflang: "en",
     },
     {
-      href: `${process.env.NEXT_PUBLIC_URL_SELF}/fr/`,
-      hreflang: "fr",
-    },
-    {
-      href: `${process.env.NEXT_PUBLIC_URL_SELF}/ja/`,
-      hreflang: "ja",
-    },
-    {
       href: `${process.env.NEXT_PUBLIC_URL_SELF}/es/`,
       hreflang: "es",
+    },
+    {
+      href: `${process.env.NEXT_PUBLIC_URL_SELF}/fr/`,
+      hreflang: "fr",
     },
     {
       href: `${process.env.NEXT_PUBLIC_URL_SELF}/pt-br/`,
       hreflang: "pt-br",
     },
+    {
+      href: `${process.env.NEXT_PUBLIC_URL_SELF}/ja/`,
+      hreflang: "ja",
+    },
   ],
-  exclude: ["/en/*", "/fr/*", "/ja/*", "/es/*", "/pt-br/*"],
+  exclude: ["/en/*", "/fr/*", "/ja/*", "/es/*", "/pt-br/*", "/404", "/500", "/dev/*"],
 };
